8a57c6c09fd20608bb70fe7265fae81c5e171051
Robert Fritzen <phantom139@phantomdev.net>
2014-03-07 13:44:29 -0600

    Added World Events
    Hallowed End's main system addition, rifting nights. First phase.

--- MBase.cs
+++ MBase.cs
@@ -11,7 +11,7 @@ namespace HallowedEnd {
         public override void OnLoad(){} //Called when the mod is first loaded. 
         public override void OnUnload(){} //Called when the mod is unloaded.
         public override void OnAllModsLoaded(){} //Called when all mods are loaded.
-        public override void PostGameDraw(SpriteBatch sb){} //Called after all game drawing is finished.
+        public override void PostGameDraw(SpriteBatch sb) { } //Called after all game drawing is finished.
         public override void ChooseTrack(ref string current){} //Called when the music track updates.
 
         public override object OnModCall(TAPI.ModBase mod, params object[] args) { return base.OnModCall(mod, args);  } //Called when another mod calls this on your mod. (used for inter-mod communicating)
